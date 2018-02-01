e680e734c18028added8f5a7792ae3bf1271b233
apolloclark <apolloclark@gmail.com>
2018-02-01 13:35:57 -0500

    removed some Elastic Beat tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -164,21 +164,6 @@ end
 
 # Define packages
 Packages = {
-  'filebeat' => {
-    version: '5.6.5'
-  },
-  'metricbeat' => {
-    version: '5.6.5'
-  },
-  'heartbeat' => {
-    version: '5.6.5'
-  },
-  'packetbeat' => {
-    version: '5.6.5'
-  },
-  'osquery' => {
-    version: '2.10.2-1.linux'
-  },
   'elasticsearch' => {
     version: '5.6.5'
   },
